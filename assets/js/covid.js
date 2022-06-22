9cca8c7257b9650e8f12c318ee0bb2c93b8e3b15
Wizctlantecuhtli <isen.mr@outlook.es>
2022-06-22 11:45:19 -0500

    Age

--- assets/js/covid.js
+++ assets/js/covid.js
@@ -85,3 +85,17 @@ window.onload = function(){
         respuestaIndigena.style.display='none';
     }
 }
+function Edad(FechaNacimiento) {
+    var fechaNace = new Date(FechaNacimiento);
+    var fechaActual = new Date()
+    var mes = fechaActual.getMonth();
+    var dia = fechaActual.getDate();
+    var año = fechaActual.getFullYear();
+
+    fechaActual.setDate(dia);
+    fechaActual.setMonth(mes);
+    fechaActual.setFullYear(año);
+
+     edad = Math.floor(((fechaActual - fechaNace) / (1000 * 60 * 60 * 24) / 365));
+     document.getElementById('entradaEdad').value = edad
+}
